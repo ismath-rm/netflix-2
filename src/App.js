ee149405635bfd28fa2f45df0ad5e6e6d390f7ce
ismath <ismathrm9@.com>
2024-03-05 17:36:45 +0530

    complete netflix-clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import { Originals,action,ComedyMovies,HorrorMovies,RomanceMovies } from './Urls';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner/>
+      <RowPost url={Originals} title='Netflix Originals' />
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={ComedyMovies} title='ComedyMovies' isSmall />
+      <RowPost url={HorrorMovies} title='HorrorMovies' isSmall />
+      <RowPost url={RomanceMovies} title='RomanceMovies' isSmall />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = 'd66d255222017acd23311e5d6b161191';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
